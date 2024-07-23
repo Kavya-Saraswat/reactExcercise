244b6962738e286bbd454b467c636df216b9fd9f
Kavya <Kavya.s@engineersmind.com>
2024-07-23 18:10:15 +0530

    useParams commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from "react";
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useParams } from "react-router-dom";
 import Login from "./components/Login";
 import Registration from "./components/Registration";
+
+function Params(){
+  const params=useParams();
+  return(
+    <div>
+      <h1>My name is {params.username}</h1>
+    </div>
+  )
+}
 
 function App() {
   return(
@@ -10,6 +19,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/registration" element={<Registration />} />
+      <Route path="/registration/:username" element={<Params/>} />
     </Routes>
   </div>);
 }
